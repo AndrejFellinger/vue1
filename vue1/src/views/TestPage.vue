3c1a1897a008cbb7f89f8163040b01104d877fa2
Andrej Fellinger <fellinger.andrej@gmail.com>
2023-02-15 21:28:02 +0100

    clear

--- vue1/src/views/TestPage.vue
+++ vue1/src/views/TestPage.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
+import { h } from 'vue';
 
+
+
+let value = 1
+
+const handleClick = () =>{
+  value +=  1;
+  console.log(value)
+}
+
+const testFunction = () =>{
+  alert("test1")
+  
+}
 </script>
 
 <template>
   <main>
     <h2>This is test page!</h2>
-    <button @click="alert('hello')">Say hello</button>
+    <button @click="handleClick">Say hello</button>
+    <button class="testhover" @mouseover="testFunction">Hover me</button>
+
+    <p>{{ value }}</p>
+    <v-if>
+      {{ value == 1 }}
+        <h1>Value sa rovna 1</h1>
+    </v-if>
 
   </main>
 </template>

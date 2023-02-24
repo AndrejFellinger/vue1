4e24b80279983f0ada01dfd25017e673d40e7048
Andrej Fellinger <fellinger.andrej@gmail.com>
2023-02-24 20:35:24 +0100

    update

--- vue1/src/views/Todo.vue
+++ vue1/src/views/Todo.vue
@@ -1,16 +1,16 @@
 <template>
-    <h1>I really made it !</h1>
-    <button @click="count ++">{{ count }}</button>
 
+    <div class="container">
+        <h1>Todo List</h1>
 
-    <ol>
-        <li v-for="kokos in list">{{ kokos }}</li>
-    </ol>
+        <ol>
+            <li v-for="kokos in list">{{ kokos }}</li>
+        </ol>
 
-    <input type="text" v-model="test" />
-    <input type="submit" value="add" @click="storeTest">
+        <input type="text" v-model="test" placeholder="start typing..." />
+        <input class="submit" type="submit" value="ADD" name="add" @click="storeTest">
+    </div>
 
-    <p>{{ test }}</p>
 
 
 </template>
@@ -20,8 +20,7 @@
 export default {
     data() {
         return {
-            count: 1,
-            list: ['hey', 'gey', "let's gooo", 'finally', 'made ITTTTTTT'],
+            list: [],
             test: ''
         }
 
@@ -35,6 +34,45 @@ export default {
 }
 </script>
 
-<style lang="">
-    
+<style scoped>
+
+    .container{
+        background: white;
+        display: grid;
+        grid-template-columns: auto;
+        color: rgb(27, 27, 27);
+        padding: 20px;
+        border-radius: 5px;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 0 15px rgba(255, 255, 255, 0.24);
+        max-height: 800px;
+        overflow: scroll;
+    }
+
+    h1{
+        text-align: center;
+        border-bottom: 1px solid rgb(212, 212, 212);
+    }
+
+    ol{
+        font-size: 18px;
+        text-align: left;
+        
+    }
+
+    input{
+        padding: 5px;
+        outline: none;
+        border: none;
+        background: rgb(212, 212, 212);
+        margin: 3px;
+        border-radius: 3px;
+    }
+
+    .submit{
+        color: green;
+        background: greenyellow;
+    }
+
 </style>
